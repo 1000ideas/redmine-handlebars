79d13870618b9c83c211422579b9c6b8c3d0bd4d
Bartek Bulat <bartek@1000i.pl>
2014-02-03 09:32:46 +0100

    Fix parsing error on settings page.

--- app/controllers/handlebars_controller.rb
+++ app/controllers/handlebars_controller.rb
@@ -2,9 +2,7 @@ class HandlebarsController < ApplicationController
   unloadable
 
   def show
-    Rails.logger.debug cookies['handlebars-hidden'].inspect
     @hidden = JSON.parse( (cookies['handlebars-hidden'] || '[]').gsub('|', ',') )
-    Rails.logger.debug @hidden
 
     respond_to do |format|
       format.html { render layout: !request.xhr? }
