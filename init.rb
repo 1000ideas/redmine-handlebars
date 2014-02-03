d42d9bc89bd8f6ca5b031fc094c7a6aebad44ef5
Bartek Bulat <bartek@1000i.pl>
2014-02-03 15:01:51 +0100

    Version Bump

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :handlebars do
   name 'HandleBars plugin'
   author '1000ideas'
   description 'This plugin allow you to see unfinished work by very handle bars'
-  version '0.0.7'
+  version '0.0.8'
   url 'http://1000i.pl'
   author_url 'http://1000i.pl'
 
