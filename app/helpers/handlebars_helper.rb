72bc5aeecf83995f456984dc53f6838ade362f66
Bartek Bulat <bartek@1000i.pl>
2014-02-25 11:35:21 +0100

    Make bar wider. Add project name before issue subject.

--- app/helpers/handlebars_helper.rb
+++ app/helpers/handlebars_helper.rb
@@ -5,12 +5,12 @@ module HandlebarsHelper
 
     timespan = (issue.estimated_hours || 0) - issue.spent_hours
     height = timespan > 0 ? (4*timespan).to_i : 1
-    
+
     if height > 16*4
       maximum = true
       height = 16*4
     end
-    
+
     class_name = [
       :hascontextmenu,
       :handlebar,
@@ -18,8 +18,8 @@ module HandlebarsHelper
       :"priority-position-#{issue.priority.position}"
     ]
     class_name << :maximum if maximum
-    
-    
+
+
     desc = strip_tags(issue.description)
     if desc.length > 300
       desc = desc.slice(0, 299).concat("&hellip;")
@@ -38,17 +38,17 @@ module HandlebarsHelper
     end
 
     content_tag tag, class: class_name, data: {issue_id: issue.id, tooltip: tooltip.to_str} do
-      items = [link_to("##{issue.id} #{issue.subject}", issue, target: '_blank')]
+      items = [link_to("##{issue.id} [#{issue.project.name}] #{issue.subject}", issue, target: '_blank')]
       items << content_tag(:span, class: :status) do
         subitems = []
         subitems << content_tag(:i, '', class: :play, title: l(:label_working_on)) if issue.respond_to?(:started?) and issue.started?
         subitems << content_tag(:i, '!', class: :'overtime', title: l(:label_overtime)) if timespan < 0
         subitems.join.html_safe
       end
-      
+
       items << content_tag(:span, '', class: "progress done-#{issue.done_ratio}")
       items << content_tag(:span, "#{timespan}h", class: "more-than-max") if maximum
-      
+
       items.join.html_safe
     end
   end
